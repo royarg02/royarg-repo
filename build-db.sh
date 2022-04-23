ce5675c5be7464b61852a8f6f83dada1d7d45efa
Anurag Roy <anuragr9847@gmail.com>
2022-04-23 16:44:52 +0530

    Add workflow to build database

---
+++ build-db.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Database build script for royarg-repo
+# - Change directory to location of package files
+# - Build database using repo-add
+
+set -e
+
+cat << END
+╔════════════════════════════════════════════════════════════════════════════╗
+║ Building the repo database...                                              ║
+╚════════════════════════════════════════════════════════════════════════════╝
+END
+
+## Arch: x86_64
+cd os/x86_64
+
+## repo-add
+## -s: signs the packages
+## -n: only add new packages not already in database
+## -R: remove old package files when updating their entry
+repo-add -s -n -R royarg-repo.db.tar.gz *.pkg.tar.zst
+
+cat << END
+╔════════════════════════════════════════════════════════════════════════════╗
+║ The database has been updated!                                             ║
+╚════════════════════════════════════════════════════════════════════════════╝
+END
