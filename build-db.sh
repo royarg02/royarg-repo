1d9aea6bc012ea40267a1980f8cd1a8d28f34226
Anurag Roy <anuragr9847@gmail.com>
2023-07-21 12:50:21 +0530

    Update database build script
    Some comments removed with cosmetic changes

--- build-db.sh
+++ build-db.sh
@@ -4,25 +4,24 @@
 # - Change directory to location of package files
 # - Build database using repo-add
 
-# Immediately exit upon fail of any command
 set -e
 
 cat << END
-╔════════════════════════════════════════════════════════════════════════════╗
-║ Building the repo database...                                              ║
-╚════════════════════════════════════════════════════════════════════════════╝
+┌─────────────────────────────────────────────────────────┐
+│ Building the repo database...                           │
+└─────────────────────────────────────────────────────────┘
 END
 
-## Arch: x86_64
+# Arch: x86_64
 cd os/x86_64
 
-## repo-add
-## List the packages version sorted, keeping lower revision number before the
-## greater one
+# repo-add
+# List the packages version sorted, keeping lower revision number before the
+# greater one
 repo-add --sign --new --remove royarg-repo.db.tar.gz $(ls --sort=version ./*.pkg.tar.zst)
 
 cat << END
-╔════════════════════════════════════════════════════════════════════════════╗
-║ The database has been updated!                                             ║
-╚════════════════════════════════════════════════════════════════════════════╝
+┌─────────────────────────────────────────────────────────┐
+│ The database has been updated!                          │
+└─────────────────────────────────────────────────────────┘
 END
